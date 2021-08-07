906bbd2c684f82ca18a3e30eafe9983de62c49f0
taslimisina <taslimisina@yahoo.com>
2021-08-07 20:01:57 +0430

    Complete rectangle constructor, Complete computeArea method

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,11 +1,14 @@
 public class Rectangle implements Shape{
+    private int width;
+    private int height;
 
     public Rectangle(int width, int height) {
-
+        this.width = width;
+        this.height = height;
     }
 
     @Override
     public int computeArea() {
-        return 0;
+        return width * height;
     }
 }
